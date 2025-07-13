795fbb82f3df2977735bfea9dde9a6f97df38bd0
haj2455 <haleybear057@gmail.com>
2025-07-13 19:25:02 -0400

    save

--- Jones_Haley_Chapter5_Project/Jones_Haley_Chapter5_Project.cpp
+++ Jones_Haley_Chapter5_Project/Jones_Haley_Chapter5_Project.cpp
@@ -2,19 +2,25 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	cout << "Pattern A         Pattern B" << endl;
+
+	for (int i = 1; i <= 10; i++) {
+
+		for (int j = 1; j <= i; j++) {
+			cout << "+";
+		}
+
+		cout << setw(14 - i);
+
+		for (int j = 1; j <= 11 - i; j++) {
+			cout << "+";
+		}
+		cout << endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
