42fc2fd73cff4dc21e442fe61af43516add2019f
haj2455 <haleybear057@gmail.com>
2025-07-13 18:55:12 -0400

    save

--- Jones_Haley_Chapter5_Project/Jones_Haley_Chapter5_Project.cpp
+++ Jones_Haley_Chapter5_Project/Jones_Haley_Chapter5_Project.cpp
@@ -7,5 +7,36 @@
 using namespace std;
 
 int main() {
+	string fileName, townName;
+	ifstream inputFile;
 
+	cout << "Enter the name of the data file: ";
+	cin >> fileName;
+
+	cout << "Enter the name of the town: ";
+	cin.ignore();
+	getline(cin, townName);
+
+	inputFile.open(fileName);
+	if (!inputFile) {
+		cout << "Error: Unable to open file '" << fileName << "'." << endl;
+		return 1;
+	}
+
+	cout << "\n" << townName << " Population Growth\n";
+	cout << "(each * represents 1,000 people)\n";
+
+	int population;
+	int year = 1900;
+
+	while (inputFile >> population) {
+		cout << year << " ";
+		for (int i = 0; i < population / 1000; ++i) {
+		}
+		cout << endl;
+		year += 20;
+	}
+
+	inputFile.close();
+	return 0;
 }
